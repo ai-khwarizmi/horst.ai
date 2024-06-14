c6b711a4d484d3bd86903a144e3a3b3d3714216b
Henrik Schwebcke <58189782+overiit@users.noreply.github.com>
2024-06-14 03:04:19 +0200

    re-add children

--- frontend/src/lib/components/CustomNode.svelte
+++ frontend/src/lib/components/CustomNode.svelte
@@ -116,48 +116,5 @@
 		</div>
 	</div>
 
-	<!-- <slot /> -->
+	<slot />
 </div>
-
-<!-- 
-<style>
-	:global(.svelte-flow__node-custom) {
-		font-size: 10px;
-		width: 180px;
-		background: #f5f5f6;
-		color: #222;
-		box-shadow:
-			0 4px 6px -1px rgb(0 0 0 / 15%),
-			0 2px 4px -1px rgb(0 0 0 / 8%);
-		border-radius: 2px;
-	}
-
-	:global(.svelte-flow__node-custom .svelte-flow__handle) {
-		top: 24px;
-		right: -15px;
-		width: 6px;
-		height: 10px;
-		border-radius: 2px;
-		background-color: #778899;
-	}
-
-	:global(.custom-node__header) {
-		padding: 8px 10px;
-		border-bottom: 1px solid #e2e8f0;
-	}
-
-	:global(.custom-node__body) {
-		padding: 10px;
-	}
-
-	:global(.custom-node__select) {
-		position: relative;
-		margin-bottom: 10px;
-	}
-
-	:global(.custom-node__select select) {
-		width: 100%;
-		margin-top: 5px;
-		font-size: 10px;
-	}
-</style> -->
