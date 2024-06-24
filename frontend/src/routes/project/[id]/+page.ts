e24971752d0bdd5b2110b31a14a75bc78c8a2f32
Till Müller <github@till.dev>
2024-06-24 15:42:40 +0200

    disable ssr for dynamic page

--- frontend/src/routes/project/[id]/+page.ts
+++ frontend/src/routes/project/[id]/+page.ts
@@ -1,6 +1,7 @@
 import type { Load } from '@sveltejs/kit';
 
 export const prerender = false;
+export const ssr = false;
 
 export const load: Load = async ({ params }) => {
 	const { id: projectId } = params;
