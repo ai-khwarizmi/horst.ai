88d218310a5ce1d61a39c950104f8de4ae29b8f2
Till Müller <github@till.dev>
2024-07-06 16:21:10 +0200

    remove share for now until we have login

--- frontend/src/lib/components/TopMenuBar.svelte
+++ frontend/src/lib/components/TopMenuBar.svelte
@@ -11,7 +11,6 @@
 		GithubIcon,
 		Settings
 	} from 'lucide-svelte';
-	import { openShareGraphModal } from './file/ShareGraph.svelte';
 	import { openApiKeySettings } from './settings/APIKeys.svelte';
 	import Button from './ui/button/button.svelte';
 	import { isMobile } from './Mobile.svelte';
@@ -90,15 +89,6 @@
 			class="pointer-events-auto"
 		>
 			<Settings class="size-3.5" />
-		</Button>
-		<Button
-			variant="secondary"
-			size="sm"
-			on:click={openShareGraphModal}
-			class="pointer-events-auto"
-		>
-			<Share2 class="mr-2 size-3.5" />
-			Share
 		</Button>
 		<div class="w-4 h-[1px] bg-gray-500"></div>
 		<Button variant="outline" size="sm" on:click={openApiKeySettings} class="pointer-events-auto">
